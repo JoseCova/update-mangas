c30149031e6f1cfde8e4dd0d9d58867f9c285ebd
JoseCova <alu0101023663@ull.edu.es>
2022-02-25 20:02:27 +0000

    Created function to connect to db - unfinished

--- update-mangas.py
+++ update-mangas.py
@@ -2,19 +2,30 @@
 # -*- encoding: utf-8 -*-
 
 from dotenv import load_dotenv
+from typing import Tuple, Optional
 import os
 
 
-def setup_env():
+# maybe change the name of the method
+def setup_env() -> Tuple[Optional[str], Optional[str]]:
     """Load environmental variables from file and return them."""
     load_dotenv()
     return os.environ.get("INTEGRATION_TOKEN"), os.environ.get("DATABASE_ID")
 
 
+def connect_to_database(token: str, db_id: str) -> None:
+    """Connect to the database and return its status."""
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-type": "application/json",
+        "Notion-Version": "2021-08-16",
+    }
+
+
 def main():
     """Execute all the other functions."""
     token, db_id = setup_env()
-    print(token, db_id)
 
 
 if __name__ == "__main__":
