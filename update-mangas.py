696c0eeb511eefb5330b83fba73ac67710a64be5
JoseCova <alu0101023663@ull.edu.es>
2022-02-27 15:08:35 +0000

    Made function to check if the connection with the database is succesfull

--- update-mangas.py
+++ update-mangas.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-from dotenv import load_dotenv
-from typing import Tuple, Optional
 import os
+from typing import Tuple, Optional
+
+import requests
+from dotenv import load_dotenv
 
 
 # maybe change the name of the method
@@ -13,8 +15,10 @@ def setup_env() -> Tuple[Optional[str], Optional[str]]:
     return os.environ.get("INTEGRATION_TOKEN"), os.environ.get("DATABASE_ID")
 
 
-def connect_to_database(token: str, db_id: str) -> None:
+def connect_to_database(token: str, db_id: str) -> bool:
     """Connect to the database and return its status."""
+
+    db_url = f"https://api.notion.com/v1/databases/{db_id}"
 
     headers = {
         "Authorization": f"Bearer {token}",
@@ -22,10 +26,19 @@ def connect_to_database(token: str, db_id: str) -> None:
         "Notion-Version": "2021-08-16",
     }
 
+    request = requests.get(db_url, headers=headers)
+
+    if request.status_code == 200:
+        return True
+
+    return False
+
 
 def main():
     """Execute all the other functions."""
     token, db_id = setup_env()
+
+    connect_to_database(token, db_id)
 
 
 if __name__ == "__main__":
