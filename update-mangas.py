990046266c6a2115f4c57a32ddc486104191efe9
JoseCova <alu0101023663@ull.edu.es>
2022-02-27 20:35:44 +0000

    Created function to query all the shonen jump mangas to the DB

--- update-mangas.py
+++ update-mangas.py
@@ -2,11 +2,13 @@
 # -*- encoding: utf-8 -*-
 
 import os
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Dict, Any
 
 import requests
 from dotenv import load_dotenv
 
+
+# TODO Meter en una clase a porque se repite mucho la url y los headers, junto con el id
 
 # maybe change the name of the method
 def setup_env() -> Tuple[Optional[str], Optional[str]]:
@@ -23,7 +25,7 @@ def connect_to_database(token: str, db_id: str) -> bool:
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-type": "application/json",
-        "Notion-Version": "2021-08-16",
+        "Notion-Version": "2022-02-22",
     }
 
     request = requests.get(db_url, headers=headers)
@@ -34,11 +36,48 @@ def connect_to_database(token: str, db_id: str) -> bool:
     return False
 
 
+def get_shonen_jump_mangas(token: str, db_id: str) -> Dict[str, Any]:
+    """Query all the shonen jump mangas and return them."""
+    db_url = f"https://api.notion.com/v1/databases/{db_id}/query"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-type": "application/json",
+        "Notion-Version": "2022-02-22",
+    }
+
+    query = {
+        "filter": {
+            "and": [
+                {"property": "Terminada", "checkbox": {"equals": False}},
+                {"property": "Revista", "select": {"equals": "Shonen Jump"}},
+            ]
+        }
+    }
+
+    mangas = requests.post(db_url, headers=headers, json=query)
+
+    return mangas.json()
+
+
+def update_shonen_jump_mangas(shonen_jump_mangas) -> None:
+    """Update the Ultimo capitulo property."""
+    print(shonen_jump_mangas)
+
+
 def main():
     """Execute all the other functions."""
     token, db_id = setup_env()
 
-    connect_to_database(token, db_id)
+    is_connection_ok = connect_to_database(token, db_id)
+
+    # Maybe esto sobra y habrá que borrar
+    if not is_connection_ok:
+        print("The connection to the database gave an error, exiting the program")
+        exit(1)
+
+    shonen_jump_mangas = get_shonen_jump_mangas(token, db_id)
+    update_shonen_jump_mangas(shonen_jump_mangas)
 
 
 if __name__ == "__main__":
