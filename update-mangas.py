76c814ba4effbf241ef2ae8bcc0d228fd7499487
JoseCova <alu0101023663@ull.edu.es>
2022-02-28 18:49:47 +0000

    Created function to yield a dict with the page id and the new chapter value

--- update-mangas.py
+++ update-mangas.py
@@ -2,13 +2,18 @@
 # -*- encoding: utf-8 -*-
 
 import os
-from typing import Tuple, Optional, Dict, Any
+from typing import Tuple, Optional, Dict, Any, List
 
 import requests
 from dotenv import load_dotenv
 
-
 # TODO Meter en una clase a porque se repite mucho la url y los headers, junto con el id
+""" 
+TODO
+Es coger de cada pagina que cumple la query el id y el ultimo numero del capi
+luego es actualizar esa pagina sumando a la propiedad correspondiente +1 
+ """
+
 
 # maybe change the name of the method
 def setup_env() -> Tuple[Optional[str], Optional[str]]:
@@ -57,12 +62,45 @@ def get_shonen_jump_mangas(token: str, db_id: str) -> Dict[str, Any]:
 
     mangas = requests.post(db_url, headers=headers, json=query)
 
-    return mangas.json()
+    return mangas.json()["results"]
 
 
-def update_shonen_jump_mangas(shonen_jump_mangas) -> None:
-    """Update the Ultimo capitulo property."""
-    print(shonen_jump_mangas)
+def add_chapter(mangas: List[Dict[str, Any]]) -> None:
+    """Add 1 to the manga Ultimo capi property and yield a dict with its id and new value."""
+    # Cuando toque actualizar 1 mirar si es arr o no y hacer el rollo
+    for m in mangas:
+        yield {
+            "page_id": m["id"],
+            "updated_chapter": m["properties"]["Ultimo capi"]["number"] + 1,
+        }
+
+
+# def update_shonen_jump_mangas(token: str, db_id: str, shonen_jump_mangas) -> None:
+#     """Update the Ultimo capitulo property."""
+#
+#     db_url = f"https://api.notion.com/v1/databases/{db_id}"
+#
+#     headers = {
+#         "Authorization": f"Bearer {token}",
+#         "Content-type": "application/json",
+#         "Notion-Version": "2022-02-22",
+#     }
+#
+#     query = {
+#         "filter": {
+#             "and": [
+#                 {"property": "Terminada", "checkbox": {"equals": False}},
+#                 {"property": "Revista", "select": {"equals": "Shonen Jump"}},
+#             ]
+#         },
+#         "properties": { "Ultimo capi":  {"number": 3 }
+#         }
+#     }
+#
+#     properties = { "properties": { "Ultimo capi": { "number": 3 }}}
+#
+#     r = requests.patch(db_url, headers= headers, json=query)
+#     print(r.text)
 
 
 def main():
@@ -77,7 +115,8 @@ def main():
         exit(1)
 
     shonen_jump_mangas = get_shonen_jump_mangas(token, db_id)
-    update_shonen_jump_mangas(shonen_jump_mangas)
+    add_chapter(shonen_jump_mangas)
+    # update_shonen_jump_mangas(token, db_id, shonen_jump_mangas)
 
 
 if __name__ == "__main__":
