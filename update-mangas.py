a8ef19bf3b02cb50ea44cf787fc87e5ee0a5574a
JoseCova <alu0101023663@ull.edu.es>
2022-03-02 09:39:05 +0000

    Changed argument behaviour, now using subparser

--- update-mangas.py
+++ update-mangas.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 import argparse
 import sys
 
+
 # TODO Meter en una clase a porque se repite mucho la url y los headers, junto con el id
 
 
@@ -20,35 +21,10 @@ def setup_env() -> Tuple[Optional[str], Optional[str]]:
     return os.environ.get("INTEGRATION_TOKEN"), os.environ.get("DATABASE_ID")
 
 
-def connect_to_database(token: str, db_id: str) -> bool:
-    """Connect to the database and return its status."""
-
-    db_url = f"https://api.notion.com/v1/databases/{db_id}"
-
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-type": "application/json",
-        "Notion-Version": "2022-02-22",
-    }
-
-    request = requests.get(db_url, headers=headers)
-
-    if request.status_code == 200:
-        return True
-
-    return False
-
-
-def get_shonen_jump_mangas(token: str, db_id: str) -> Dict[str, Any]:
+def get_shonen_jump_mangas(db_id: str, headers: Dict[str, str]) -> Dict[str, Any]:
     """Query all the shonen jump mangas and return them."""
 
     db_url = f"https://api.notion.com/v1/databases/{db_id}/query"
-
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-type": "application/json",
-        "Notion-Version": "2022-02-22",
-    }
 
     query = {
         "filter": {
@@ -76,17 +52,14 @@ def add_chapter(mangas: List[Dict[str, Any]]) -> Generator[Dict[str, Any], None,
         }
 
 
-def update_shonen_jump_mangas(token: str, shonen_jump_mangas: Dict[str, Any]) -> None:
+def update_shonen_jump_mangas(
+    headers: Dict[str, str], shonen_jump_mangas: Dict[str, Any]
+) -> None:
     """Update the Ultimo capitulo property."""
-
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-type": "application/json",
-        "Notion-Version": "2022-02-22",
-    }
 
     for sjm in add_chapter(shonen_jump_mangas):
         page_url = f"https://api.notion.com/v1/pages/{sjm['page_id']}"
+
         properties_to_update = {
             "properties": {"Ultimo capi": {"number": sjm["updated_chapter"]}}
         }
@@ -103,40 +76,32 @@ def update_shonen_jump_mangas(token: str, shonen_jump_mangas: Dict[str, Any]) ->
         )
 
 
+# TODO cambiar docstring
 def main():
     """Execute all the other functions."""
-    token, db_id = setup_env()
 
-    is_connection_ok = connect_to_database(token, db_id)
+    parser = argparse.ArgumentParser(
+        description="Update mangas DB in Notion",
+        usage="To see each subcommand usage please execute python update-mangas [all-shonen-jump | update-single] -h\n",
+    )
+    subparsers = parser.add_subparsers()
 
-    # Maybe esto sobra y habrá que borrar
-    if not is_connection_ok:
-        print("The connection to the database gave an error, exiting the program")
-        exit(1)
-
-    shonen_jump_mangas = get_shonen_jump_mangas(token, db_id)
-
-    update_shonen_jump_mangas(token, shonen_jump_mangas)
-
-
-if __name__ == "__main__":
-
-    # TODO meter esto en main
-    parser = argparse.ArgumentParser(description="Update mangas DB in Notion")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "-s",
-        "--all-shonen-jump",
+    parse_all_shonen_jump = subparsers.add_parser(
+        "all-shonen-jump",
         help="Add 1 to the Ultimo capi property to all the Shonen Jump mangas",
-        action="store_true"
     )
-    group.add_argument(
-        "-u",
-        "--single-update",
-        type=str,
-        metavar="",
-        help="Add 1 to the Ultimo capi property to the manga given as a parameter",
+    parse_all_shonen_jump.add_argument(
+        "-i",
+        "--ignore",
+        help="Dont update the Ultimo capi property in the manga passed as an argument",
     )
+
+    parse_single_manga = subparsers.add_parser(
+        "single-update",
+        help="Add 1 to the Ultimo capi property to the manga given as an argument",
+        usage="e",
+    )
+
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
@@ -145,3 +110,19 @@ if __name__ == "__main__":
         )
         parser.print_help(sys.stderr)
         exit(1)
+
+    token, db_id = setup_env()
+
+    notion_headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-type": "application/json",
+        "Notion-Version": "2022-02-22",
+    }
+
+    # shonen_jump_mangas = get_shonen_jump_mangas(db_id, notion_headers)
+
+    # update_shonen_jump_mangas(notion_headers, shonen_jump_mangas)
+
+
+if __name__ == "__main__":
+    main()
